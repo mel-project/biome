7d6f09f4db18db7f4a01c38da6d320aa911ae6ef
Meade Kincke <thedarkula2049@gmail.com>
2021-10-06 10:36:30 -0600

    Fixed a bit of copy and paste.

--- caddy-loki/plan.sh
+++ caddy-loki/plan.sh
@@ -4,7 +4,7 @@ pkg_version=2.4.5
 pkg_maintainer="Meade Kincke <meade@themelio.org>"
 pkg_source="https://github.com/caddyserver/caddy/releases/download/v${pkg_version}/caddy_${pkg_version}_linux_amd64.tar.gz"
 pkg_shasum=7b28c989091a03c6aa5e397eb37d3e2c7ed3826c894534e30fd2de2f4084e770
-pkg_description="This is a load balancer for prometheus."
+pkg_description="This is a load balancer for loki."
 pkg_upstream_url=https://caddyserver.com
 pkg_deps=(
   core/gettext

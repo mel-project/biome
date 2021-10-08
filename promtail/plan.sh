185e11b91d2ebd7995af00e3178847d7c1aad305
Meade Kincke <thedarkula2049@gmail.com>
2021-10-08 11:39:10 -0600

    Updated the promtail plan.

--- promtail/plan.sh
+++ promtail/plan.sh
@@ -11,24 +11,36 @@ pkg_shasum="c71174a2fbb7b6183cb84fc3a5e328cb4276a495c7c0be8ec53c377ec0363489"
 loki_pkg_dir="${HAB_CACHE_SRC_PATH}/${pkg_name}-${pkg_version}"
 loki_build_dir="${loki_pkg_dir}/src/${pkg_source}"
 pkg_build_deps=(
+  core/bash
   core/busybox-static
-  core/cacerts
-  core/coreutils
   core/gcc
   core/git
   themelio/go
   core/make
+  core/patchelf
 )
 pkg_deps=(
+  core/cacerts
+  core/coreutils
   core/glibc
+  core/libgcrypt
+  core/libgpg-error
   core/systemd
+  core/tzdata
+  core/zstd
 )
 pkg_bin_dirs=(bin)
 pkg_binds_optional=(
   [loki]="http_port"
 )
+pkg_exports=(
+  [http_port]=http_port
+  [grpc_port]=grpc_port
+)
+pkg_exposes=(http_port grpc_port)
 pkg_svc_user="root"
 pkg_svc_group="$pkg_svc_user"
+
 
 do_setup_environment() {
   export GOPATH="${HAB_CACHE_SRC_PATH}/${pkg_dirname}"
@@ -57,26 +69,13 @@ do_build() {
   mkdir -p /etc/ssl
   ln -s "$(pkg_path_for core/cacerts)/ssl/certs/cacert.pem" /etc/ssl/ca-bundle.pem
 
-  LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:$(pkg_path_for core/systemd)/lib:$(pkg_path_for core/glibc)/lib"
-  export LD_LIBRARY_PATH
+  sed -e "s#SHELL = /usr/bin/env bash#SHELL = $(pkg_path_for core/coreutils)/bin/env bash#" -i Makefile
+  fix_interpreter "tools/image-tag" core/coreutils bin/env
 
-#  go build \
-#  -trimpath \
-#  -buildmode=pie \
-#  -mod=readonly \
-#  -modcacherw \
-#  -ldflags " \
-#     -X github.com/grafana/loki/pkg/util/build.Version=$pkg_version
-#     -X github.com/grafana/loki/pkg/util/build.BuildDate=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
-#     -linkmode external
-#     -extldflags \"${LDFLAGS}\"" \
-#  ./clients/cmd/promtail
+  make touch-protos
+#  CGO_CFLAGS="-I$(pkg_path_for core/systemd)/include" make BUILD_IN_CONTAINER=false promtail
 
   CGO_CFLAGS="-I$(pkg_path_for core/systemd)/include" go build -trimpath -buildmode=pie -mod=readonly -modcacherw -ldflags "-X github.com/grafana/loki/pkg/util/build.Version=$pkg_version -X github.com/grafana/loki/pkg/util/build.BuildDate=$(date -u +'%Y-%m-%dT%H:%M:%SZ') -linkmode external -extldflags \"${LDFLAGS}\"" ./clients/cmd/promtail
-
-
-#  CGO_ENABLED=1 CGO_CFLAGS=${CFLAGS} go build ./clients/cmd/promtail
-
 
   rm -rf /etc/ssl
 
@@ -85,7 +84,7 @@ do_build() {
 
 do_install() {
   pushd "${loki_pkg_dir}/src/github.com/grafana/loki" > /dev/null || exit 1
+#  install -Dm755 clients/cmd/promtail/promtail "${pkg_prefix}/bin/promtail"
   install -Dm755 promtail "${pkg_prefix}/bin/promtail"
   popd > /dev/null || exit 1
 }
-

cf0e23a225ab92d4e6943222d5002b95d7caab60
Meade Kincke <thedarkula2049@gmail.com>
2021-10-21 09:56:38 -0600

    Upgraded the grafana version, created a centralised dashboards directory.

--- grafana/plan.sh
+++ grafana/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=grafana
 pkg_origin=themelio
-pkg_version=8.2.1
+pkg_version=8.2.2
 pkg_maintainer="Meade Kincke <meade@themelio.org>"
 pkg_license=("Apache-2.0")
 pkg_upstream_url=https://grafana.com/
 pkg_source="https://dl.grafana.com/oss/release/${pkg_name}-${pkg_version}.linux-amd64.tar.gz"
 pkg_filename="${pkg_name}-${pkg_version}.linux-amd64.tar.gz"
-pkg_shasum=e238ec1f78369fc8f9c094584223d763a6e86a83cf33bcb7b0d750341dc2e988
+pkg_shasum=573bb0c94353fa6e41c9ae0bcfdb63dbdc363a2b1db0ec9a272b7a22da7b3884
 pkg_deps=(
   core/glibc
   core/bash

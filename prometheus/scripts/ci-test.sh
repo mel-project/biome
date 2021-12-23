cc4ae2c7547eb5858dd4922a9de84acf00de85b3
Meade Kincke <thedarkula2049@gmail.com>
2021-12-23 11:50:02 -0800

    Trying a fix for the prometheus CI.

--- prometheus/scripts/ci-test.sh
+++ prometheus/scripts/ci-test.sh
@@ -2,45 +2,39 @@
 
 set -ex
 
+SCRIPTS_DIRECTORY="$(dirname "${0}")"
+PLAN_DIRECTORY="$(dirname "${SCRIPTS_DIRECTORY}")"
+
+sudo bio pkg install --binlink core/bats
+sudo bio pkg install --binlink core/curl
+sudo bio pkg install --binlink core/net-tools
+
 wget -q https://github.com/themeliolabs/artifacts/raw/master/htmlq
 chmod +x htmlq
 sudo mv htmlq /bin
 
-htmlq --help
+source "${PLAN_DIRECTORY}/plan.sh"
 
-#SCRIPTS_DIRECTORY="$(dirname "${0}")"
-#PLAN_DIRECTORY="$(dirname "${SCRIPTS_DIRECTORY}")"
-#
-#sudo bio pkg install --binlink core/bats
-#sudo bio pkg install --binlink core/curl
-#sudo bio pkg install --binlink core/net-tools
-#
-#wget -q https://github.com/themeliolabs/artifacts/raw/master/htmlq
-#chmod +x htmlq
-#sudo mv htmlq bin
-#
-#source "${PLAN_DIRECTORY}/plan.sh"
-#
-#sudo bio sup run &
-#
-#bio pkg build "${pkg_name}"
-#
-#source results/last_build.env
-#
-#sudo bio pkg install --binlink --force "results/${pkg_artifact}"
-#
-#sudo useradd hab -s /bin/bash -p '*'
-#
-#sudo bio svc load "${pkg_ident}"
-#
-#echo "Sleeping for 5 seconds for the service to start."
-#sleep 5
-#
-#if bats "${SCRIPTS_DIRECTORY}/test.bats"; then
-#  sudo rm -rf /bin/htmlq
-#  sudo bio svc unload "${pkg_ident}"
-#else
-#  sudo rm -rf /bin/htmlq
-#  sudo bio svc unload "${pkg_ident}"
-#  exit 1
-#fi
+sudo bio sup run &
+
+bio pkg build "${pkg_name}"
+
+source results/last_build.env
+
+sudo bio pkg install --binlink --force "results/${pkg_artifact}"
+
+sudo useradd hab -s /bin/bash -p '*'
+
+sudo bio svc load "${pkg_ident}"
+
+echo "Sleeping for 5 seconds for the service to start."
+sleep 5
+
+if bats "${SCRIPTS_DIRECTORY}/test.bats"; then
+  sudo rm -rf /bin/htmlq
+  sudo bio svc unload "${pkg_ident}"
+else
+  sudo rm -rf /bin/htmlq
+  sudo bio svc unload "${pkg_ident}"
+  exit 1
+fi
